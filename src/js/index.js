47106e670430e2f86fd2aeac50e5413ed453c2e3
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-08-04 22:21:47 +1200

    added page numbers
    added page numbers

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,43 @@
-import axios from 'axios';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+//Global state of app
+const state = {};
 
+const controlSearch = async () => {
+	//get query from view
+	const query = searchView.getInput();
+
+	if (query) {
+		//new search object and add to state
+		state.search = new Search(query);
+
+		//3 prepare UI for results
+		searchView.clearInput();
+		searchView.clearResults();
+		renderLoader(elements.searchRes);
+		//4 Search for recipes
+		await state.search.getResults();
+
+		//5 render results on UI
+		clearLoader();
+		searchView.renderResults(state.search.result);
+	}
+};
+
+elements.searchForm.addEventListener('submit', e => {
+	e.preventDefault();
+	controlSearch();
+});
 
 //ec10b3066d895b5626a7f081ef8199de
 //http://food2fork.com/api/search
 
-async function getResults(query) {
-    const key = 'ec10b3066d895b5626a7f081ef8199de';
-    const res = await axios(`http://food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);   
-}
-
-getResults('pizza');
+elements.searchResPages.addEventListener('click', e => {
+	const btn = e.target.closest('.btn-inline');
+	if (btn) {
+		const goToPage = parseInt(btn.dataset.goto, 10);
+		searchView.clearResults();
+		searchView.renderResults(state.search.result, goToPage);
+	}
+});
