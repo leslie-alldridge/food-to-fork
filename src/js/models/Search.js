6119a9b9ec2325b38fdcb6cbf36130e5778f19e8
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-08-05 12:58:39 +1200

    added multiple controllers final commit
    added multiple controllers final commit

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
-
+import { key } from '../config';
 export default class Search {
 	constructor(query) {
 		this.query = query;
 	}
-
-	async getResults(query) {
-		const key = 'ec10b3066d895b5626a7f081ef8199de';
+	async getResults() {
 		try {
 			const res = await axios(`http://food2fork.com/api/search?key=${key}&q=${this.query}`);
 			this.result = res.data.recipes;
